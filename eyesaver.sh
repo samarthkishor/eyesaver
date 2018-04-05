eafbfa6f34f856d52caa42d64bd53454d3d23a3d
Samarth Kishor <samarthkishor1@gmail.com>
2018-04-05 18:28:06 -0400

    Updated the script to follow the 20-20-20 rule

--- eyesaver.sh
+++ eyesaver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-osascript -e 'tell app "System Events" to display dialog "Save your eyes! Look away from the screen for 20 seconds"'
+osascript -e 'tell app "System Events" to display dialog "Save your eyes! Look away from the screen for 20 seconds (preferably at something 20 feet away)"'
