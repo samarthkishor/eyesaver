5b0547d83c57c64d6025977369b9f5539020cb6c
Samarth Kishor <samarthkishor1@gmail.com>
2020-04-06 14:20:59 -0400

    Use applescript file in eyesaver.sh

--- eyesaver.sh
+++ eyesaver.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-osascript -e 'display dialog "Save your eyes! Look away from the screen for 20 seconds (preferably at something 20 feet away)" with title "eyesaver" buttons {"Ignore"} giving up after 20'
+osascript eyesaver_dialog.applescript > /dev/null
